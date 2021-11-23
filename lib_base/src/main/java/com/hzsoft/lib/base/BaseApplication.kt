2faff5ccb69affb8a2987eb832ebdf047d8908b1
zhouhuan <zhouhuan88888@163.com>
2021-11-23 13:40:31 +0800

    feature: 初始化吐司工具

--- lib_base/src/main/java/com/hzsoft/lib/base/BaseApplication.kt
+++ lib_base/src/main/java/com/hzsoft/lib/base/BaseApplication.kt
@@ -3,6 +3,7 @@ package com.hzsoft.lib.base
 import android.app.Application
 import android.content.Context
 import com.alibaba.android.arouter.launcher.ARouter
+import com.hzsoft.lib.base.utils.ToastUtil
 import com.hzsoft.lib.log.KLog
 
 /**
@@ -21,6 +22,8 @@ open class BaseApplication : Application() {
             ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this) // 尽可能早，推荐在Application中初始化
+
+        ToastUtil.init(this)
     }
 
     companion object {
