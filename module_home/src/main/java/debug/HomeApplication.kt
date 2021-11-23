797f26d809eba215f45f2dd3b455ae5bbe1ac1d8
zhouhuan <zhouhuan88888@163.com>
2021-11-23 21:04:57 +0800

    refactor: 优化

---
+++ module_home/src/main/java/debug/HomeApplication.kt
@@ -0,0 +1,19 @@
+package debug
+
+import android.app.Application
+import com.hzsoft.lib.net.config.NetConfig
+import com.hzsoft.lib.common.URL_BASE
+
+/**
+ * Description: <><br>
+ * Author:      mxdl<br>
+ * Date:        2018/12/27<br>
+ * Version:     V1.0.0<br>
+ * Update:     <br>
+ */
+class HomeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ module_me/src/main/java/debug/MeApplication.kt
@@ -0,0 +1,17 @@
+package debug
+
+import android.app.Application
+
+/**
+ *
+ *
+ * @author zhouhuan
+ * @Data 2020/12/3
+ */
+class MeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
