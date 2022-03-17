a13eade4bd22dcf36b889ef853fcc7ecac4aa0db
zhouhuan <zhouhuan88888@163.com>
2022-03-17 20:54:31 +0800

    fixed: <module_me> 增加ViewModel

---
+++ module_me/src/main/java/com/hzsoft/module/me/viewmodel/MainMeViewModel.kt
@@ -0,0 +1,6 @@
+package com.hzsoft.module.me.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import com.hzsoft.lib.base.mvvm.viewmodel.BaseViewModel
+
+class MainMeViewModel(state: SavedStateHandle) : BaseViewModel()
